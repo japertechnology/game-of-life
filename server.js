d82eaded8ad464c3e349922b923eb08048a7f56e
Thiago do N. Ferreira <thiagodnf@gmail.com>
2022-07-10 09:28:31 -0400

    updated jquery

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + "/"));
 
-var port = process.env.PORT || 3000; 
+var port = process.env.PORT || 3000;
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log("Magic happens on port " + port);
