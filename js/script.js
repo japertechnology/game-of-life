d45317db6647d9b659e4ce32e79d98e560f63938
Thiago do N. Ferreira <thiagodnf@gmail.com>
2022-07-10 11:14:02 -0400

    updated the bootstrap version! Now it is using version 5.1

--- js/script.js
+++ js/script.js
@@ -7,29 +7,29 @@ $(function() {
 	var ctx = canvas.getContext("2d");
 	var animationSpeed = 90;
 	var intervalID = 0;
-	
+
 	var maxWidth = 80;
 	var minWidth = 6;
 	var width = 20;
-	
+
 	var isPressedButton = false;
 	var isPressedKey = false;
 	var isRunning = false;
 	var isShowGrid = true;
 	var isShowingSettingsPanel = false;
-	
+
 	game.onChange = function(line,column,state){
 		if(state == "alive"){
 			drawCell(line,column,"black");
 		}else if(state == "dead"){
 			drawCell(line,column,"white");
-		}		
+		}
 	};
-	
+
 	game.onInit = function(){
 		draw();
 	};
-		
+
 	function drawCell(line,column,color){
 		ctx.beginPath();
 		ctx.fillStyle = color;
@@ -42,20 +42,20 @@ $(function() {
 		ctx.strokeStyle = color;
 		ctx.lineWidth = lineWidth;
 		ctx.moveTo(x0, y0);
-		ctx.lineTo(x1, y1);			
+		ctx.lineTo(x1, y1);
 		ctx.stroke();
 	}
-	
+
 	function drawGrid(){
 		for(var i=0;i<game.maxColumns;i++){
 			drawLine(width+i*width,0,width+i*width,game.maxLines*width,1,"black");
 		}
-		
+
 		for(var i=0;i<game.maxLines;i++){
 			drawLine(0,width+i*width,game.maxColumns*width,width+i*width,1,"black");
 		}
 	}
-	
+
 	function drawCells(){
 		for(var i=0;i<game.maxLines;i++){
 			for(var j=0;j<game.maxColumns;j++){
@@ -66,28 +66,28 @@ $(function() {
 					drawCell(i,j,"white");
 				}
 			}
-		}	
+		}
 	}
-	
-	function draw(){		
+
+	function draw(){
 		//Clear the screen before draw
-		ctx.clearRect (0 , 0 , canvas.width,canvas.height );		
-		
+		ctx.clearRect (0 , 0 , canvas.width,canvas.height );
+
 		if(isShowGrid){
 			drawGrid();
 		}
-	
-		drawCells();	
+
+		drawCells();
 	}
-	
+
 	function enableAllButtons(){
-		$(".action-button").removeAttr("disabled");		
+		$(".toolbar .btn").removeClass("disabled").removeAttr("disabled");
 	}
 
 	function disableAllButtons(){
-		$(".action-button").attr("disabled","disabled");		
+		$(".toolbar .btn").addClass("disabled").attr("disabled","disabled");
 	}
-	
+
 	function getClickPosition(){
 		if(mouseX >= game.maxColumns*width){
 			return ;
@@ -96,29 +96,27 @@ $(function() {
 		if(mouseY >= game.maxLines*width){
 			return;
 		}
-		
+
 		return {
 			line: Math.floor(mouseY/width),
 			column: Math.floor(mouseX/width)
 		};
 	}
-	
+
 	function toggleCell(line,column){
 		if(game.isAlive(line,column)){
 			game.kill(line,column);
 		}else{
-			game.born(line,column);			
+			game.born(line,column);
 		}
 	}
-	
+
 	$( window ).resize(function() {
-		resizeCanvas();		
+		resizeCanvas();
 	});
-	
+
 	//Remove context menu when user click with right button
-	$(document).bind("contextmenu",function(e){
-		return false;
-	}).keydown(function(event) {
+	$(document).keydown(function(event) {
 		//Pressing the CRTL KEY
 		if(event.keyCode == 17){
 			isPressedKey = true;
@@ -128,86 +126,94 @@ $(function() {
 				for(var j=0;j<game.maxColumns;j++){
 					if(game.cells[i][j] == 1){
 						console.log("game.born("+i+","+j+");");
-					}					
+					}
 				}
-			}	
+			}
 		}
 	}).keyup(function(event) {
 		isPressedKey = false;
 	}).click(function(e) {
         if(e.target.id=="close-speed-popover" ){
-			$("#speed-popover").popover("hide");		
+			$("#speed-popover").popover("hide");
         }
 	});
-		
+
 	$( "canvas" ).mousemove(function( event ) {
 		var rect = canvas.getBoundingClientRect();
-	
+
 		mouseX = event.clientX - rect.left;
 		mouseY = event.clientY - rect.top;
-		
+
 		if(isPressedButton && isPressedKey){
 			var pos = getClickPosition();
-				
+
 			if(pos == null){
 				return;
 			}
-			
-			game.born(pos.line,pos.column);			
+
+			game.born(pos.line,pos.column);
 		}
 	}).click(function(event) {
 		event.preventDefault();
-		
+
 		var pos = getClickPosition();
-		
+
 		if(pos == null || isPressedKey){
 			return;
 		}
-		
+
 		toggleCell(pos.line,pos.column);
 	}).mousedown(function(event) {
 		event.preventDefault();
-		isPressedButton = true;			
+		isPressedButton = true;
 	}).mouseup(function(event) {
-		event.preventDefault();		
+		event.preventDefault();
 		isPressedButton = false;
 	}).bind("mousewheel", function(e){
 		if(e.originalEvent.wheelDelta < 0) {
 			zoomOut();
 		}else {
 			zoomIn();
-		}		
+		}
 	    //prevent page fom scrolling
 	    return false;
 	}).css("cursor","pointer");
-	
+
 	$("#about").click(function(event) {
 		$("#myModal").modal("show");
 	});
 
 	$("#startAndStop").click(function(event) {
-		isRunning = !isRunning;
-	
+
+        isRunning = !isRunning;
+
 		if(isRunning){
-			$("#startAndStop").html("<span class=\"glyphicon glyphicon-stop\"></span> Stop");
-			disableAllButtons();
+
+            $("#startAndStop i").addClass("bi-stop-fill").removeClass("bi-play-fill");
+            $("#startAndStop span").html("Stop");
+
+			// disableAllButtons();
+
 			start();
 		}else{
-			$("#startAndStop").html("<span class=\"glyphicon glyphicon-play\"></span> Play");
-			enableAllButtons();			
-		}		
+
+            $("#startAndStop i").addClass("bi-play-fill").removeClass("bi-stop-fill");
+            $("#startAndStop span").html("Play");
+
+            // enableAllButtons();
+		}
 	});
-	
+
 	$("#zoom-in").click(function(event) {
-		zoomIn();	
+		zoomIn();
 	});
-	
+
 	$("#zoom-out").click(function(event) {
 		zoomOut();
 	});
-	
+
 	$("#step").click(function(event) {
-		game.step();	
+		game.step();
 	});
 
 	$("#clear-all").click(function(event) {
@@ -215,11 +221,11 @@ $(function() {
 			resetAll();
 		}
 	});
-	
+
 	$("#settings").click(function(event) {
-		$("#settings-modal").modal("show");		
+		$("#settings-modal").modal("show");
 	});
-	
+
 	$("#show-grid-select" ).change(function() {
 		if($( this ).val() == 1){
 			isShowGrid = true;
@@ -228,25 +234,31 @@ $(function() {
 		}
 		draw();
 	});
-	
-	$("#slider-speed").slider({
-		min: 1,
-		max: 100,
-		step: 1,
-		value: animationSpeed
-	}).on("slide", function (ev) {
-		animationSpeed = ev.value;		
+
+    $("#slider-speed").on("change", function() {
+        animationSpeed = $( this ).val();
     });
-	
+
+    $(".navbar-nav li a").on("click", function(){
+
+        if(!$( this ).hasClass("dropdown-toggle")){
+            $(".navbar-collapse").collapse("hide");
+        }
+    });
+
 	function resizeCanvas() {
+
+        console.debug("Resizing canvas");
+
 		canvas.width = $("#panel").width();
-		canvas.height = $(window).height() - $("#navbar").height() - $(".sec-nav-bar").height()-60;
+		canvas.height = $(window).height() - $(".navbar").height() - $(".toolbar").height()-60;
+
 		draw();
 	}
-	
+
   	function resetAll(){
 		game.init();
-	}	
+	}
 
 	function zoomIn(){
 		//scroll up
@@ -255,7 +267,7 @@ $(function() {
 		}
 		draw();
 	}
-	
+
 	function zoomOut(){
 		//scroll down
 		if(width >= minWidth){
@@ -265,14 +277,15 @@ $(function() {
 	}
 
 	function start(){
+
 		clearInterval(intervalID);
-		
+
 		game.step();
-				
+
 		if(isRunning){
 			intervalID = setInterval(start, (100-animationSpeed)*10);
 		}
 	}
 
 	resizeCanvas();
-});	
+});
